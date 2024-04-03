6b1c49bbd69966baeabdcc14bafbe8aa52f944cd
Oshawhat <alannobledalL@gmail.com>
2024-04-03 08:29:41 -0500

    add info to team interface

--- src/app/team.ts
+++ src/app/team.ts
@@ -2,24 +2,51 @@ export interface Team {
     number: number;
     name: string;
 
-    averageScore?: number;
-    autoScore?: number;
-    teleOpScore?: number;
-    endGameScore?: number;
+    driveInfo?: string;
+    armInfo?: string;
+    intakeInfo?: string;
 
     teamProp?: boolean;
-    objectDetection?: boolean;
+    objectDetection?: ObjectDetection;
     purplePlacement?: PixelPlacement;
     yellowPlacement?: PixelPlacement;
+    autoWhites?: number;
+    autoScore?: number;
+    autoNotes?: string;
+
+    mosaicsScored?: number
+    setLinesReached?: number
+    pixelsScored?: number
+    teleOpScore?: number;
+    teleopNotes?: string;
 
     canHang?: boolean;
     hangTime?: number;
     droneScore?: number;
+    endGameScore?: number;
+    endGameNotes?: string;
 
+    averageScore?: number;
+
+}
+
+export enum ObjectDetection {
+    reliable = 0,
+    mostlyReliable = 1,
+    unreliable = 2,
+}
+
+export enum DriveType {
+    mecanum = 0,
+    tank = 1,
+    xDrive = 2,
+    hDrive = 3,
+    other = 4
 }
 
 export enum PixelPlacement {
     correctPlacement = 0,
-    backDrop = 1,
-    backStage = 2,
+    sometimesCorrect = 1,
+    backDrop = 2,
+    backStage = 3,
 }
